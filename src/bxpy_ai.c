f75d15d0f95671da22343ef9eca6a67ef6ae845c
Pawkkie <moog272@gmail.com>
2026-07-01 00:43:37 -0400

    fix ai assert

--- src/bxpy_ai.c
+++ src/bxpy_ai.c
@@ -92,6 +92,7 @@ void BXPY_InitializeAIStructs(void)
 {
     gAiThinkingStruct = AllocZeroed(sizeof(*gAiThinkingStruct));
     gAiLogicData = AllocZeroed(sizeof(*gAiLogicData));
+    gBattleHistory = AllocZeroed(sizeof(*gBattleHistory));
 }
 
 void BXPY_ClearAIData(void)
@@ -232,7 +233,7 @@ static void BXPY_CalcAiBattlerDamage(enum BattlerId battlerAtk, enum BattlerId b
 static bool32 BXPY_CanCandidateWin1v1(enum BattlerId battler, enum BattlerId opposingBattler)
 {
     enum Move move, opposingMove, bestOpposingMove = MOVE_NONE, bestOpposingPriorityMove = MOVE_NONE;
-    u32 hitsToKO = 0, hitsToKOOpponent = 0, minHitsToKO = gBattleMons[battler].hp, minHitsToKOPriority = gBattleMons[battler].hp;
+    u32 hitsToKO = gBattleMons[battler].hp, hitsToKOOpponent = gBattleMons[opposingBattler].hp, minHitsToKO = gBattleMons[battler].hp, minHitsToKOPriority = gBattleMons[battler].hp;
     bool32 canBattlerWin1v1 = FALSE, isBattlerFirst, isBattlerFirstPriority;
 
     // Get max damage mon could take
